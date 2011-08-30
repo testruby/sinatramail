5073f10b26423defe3fa095c865c922af5e748f1
TestRuby <testruby@rambler.ru>
2011-08-30 12:35:36 +0400

    Добавлены модели имен и списков. Подключен гем Faker для первичного заполнения базы. Внесены тестовые данные. Нет полей времени. Вырисовывается схема 	:имена 	:списки 	:письма 	:вложения при подготовке рассылки создается письмо и выбираются списки для отправки.

--- base_setup.rb
+++ base_setup.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sequel'
 require 'mime/types'
+require 'faker' #just for test
 DB = Sequel.sqlite("database.db")
 DB.create_table?(:emails) do
 	primary_key :id
@@ -13,6 +14,27 @@ DB.create_table?(:attachs) do
 	String :types
 	column :file, File
 	foreign_key :email_id, :emails
+end
+DB.create_table?(:names)do
+	primary_key :id
+	String :names
+	String :email, :empty => false, :unique => true
+end
+DB.create_table?(:lists) do
+	primary_key :id
+	String :title
+	String :description, :text => true
+end
+DB.create_table?(:lists_names) do
+	primary_key :id
+	foreign_key :name_id, :names
+	foreign_key :list_id, :lists
+end
+class Name < Sequel::Model(:names)
+	many_to_many :lists
+end
+class List < Sequel::Model(:lists)
+	many_to_many :names
 end
 class Email < Sequel::Model
 	one_to_many :attachs
@@ -30,6 +52,23 @@ end
 	y.file = File.open("base_setup.rb", "r").read
 	y.save
 end
-@a.add_attachs(@b)
+@a.add_attach(@b)
 @a.save
+1.upto(10) do
+	name = Name.new
+	name.names = Faker::Name.name
+	name.email = Faker::Internet.email
+	name.save
+end
+1.upto(2) do |x|
+	list = List.new(:title => x.to_s, :description => "not info for this list")
+	list.save 
+end
+@list1 = List.find(:id => 1)
+@names = Name.all
+@names.each do |z|
+	@list1.add_name(z)
+	z.save
+	@list1.save
+end
 
